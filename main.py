0a5a499c8121840bdfaba97db94d519766589c94
McGengar <macwegrzyn@gmail.com>
2023-04-10 11:36:44 +0200

    Added Movement Resrtictions
    Added function checkPos to check if there is white pawn in selected position and function checkMovesWhitePawn to find possible moves (captures not implemented yet).
    In main file added while loops to check if moves you make are legal

--- main.py
+++ main.py
@@ -19,7 +19,15 @@ if __name__ == "__main__":
         draw(board)
         print()
         pos=input("Podaj pole pionka: ")
+        possibleMoves = checkMovesWhitePawn(board,pos)
+        while checkPos(board,pos)==False or len(possibleMoves)==0:
+            print("Na podanym polu nie znajduje sie pionek ktorym mozes sie ruszyc!")
+            pos=input("Podaj pole pionka: ")
+            possibleMoves = checkMovesWhitePawn(board,pos)
         target=input("Podaj gdzie go chcesz ruszyc pionka: ")
+        while target.lower()not in(possibleMoves):
+            print("Podano nieprawidlowe pole!")
+            target=input("Podaj gdzie go chcesz ruszyc pionka: ")
         board = move(board,pos,target)
         
             
--- pawnMove.py
+++ pawnMove.py
@@ -8,3 +8,35 @@ def move(board, pos, target):
     board[posy][posx]=0
     board[targety][targetx]=pawn
     return board
+
+
+def checkPos(board,pos):
+    posx=int(ord(pos[0].lower())-97)
+    posy=8-int(pos[1])
+    if board[posy][posx]==1:
+        return True
+    else:
+        return False
+    
+def checkMovesWhitePawn(board,pos):
+    possibleMoves = set()
+    posx=int(ord(pos[0].lower())-97)
+    posy=8-int(pos[1])
+    #print(posx,posy)
+    if posy>0:
+        if posx>0 and board[posy-1][posx-1]==0:
+            targetx=chr(posx-1+97)
+            targety=8-posy+1
+            #print(posx-1,posy-1)
+            possibleMoves.add(targetx+str(targety))
+        if posx<7 and board[posy-1][posx+1]==0:
+            targetx=chr(posx+1+97)
+            targety=8-posy+1
+            #print(posx+1,posy-1)
+            possibleMoves.add(targetx+str(targety))
+    #print(possibleMoves)
+    return possibleMoves
+
+
+
+    
